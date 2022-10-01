a6bc1026c51f0aef29d8664128d9b2b3af0bd6a4
centipeda <joshua.ka.cepeda@gmail.com>
2022-10-01 17:49:39 +0000

    add args to run script

--- run.py
+++ run.py
@@ -2,7 +2,33 @@
 
 import waitress
 import pastebin.app
+import sys
+
+default_port = 5260
+
+def usage():
+    print(f'usage: {sys.argv[0]} [-h] [-p port]')
+    sys.exit(1)
+
+def run(port):
+    app = pastebin.app.create_app()
+    waitress.serve(app, port=port)
+
+def main():
+    if len(sys.argv) == 1:
+        port = default_port
+    else:
+        args = sys.argv[1:]
+        for i,arg in enumerate(args):
+            if arg == '-h':
+                usage()
+            if arg == '-p':
+                try:
+                    port = int(args[i+1])
+                except Exception:
+                    usage()
+    run(port)
 
 if __name__ == "__main__":
-    app = pastebin.app.create_app()
-    waitress.serve(app, port=5260)
+    main()
+
